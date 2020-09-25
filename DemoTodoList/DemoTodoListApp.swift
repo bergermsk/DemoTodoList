338ecd2e335ebbb6e0828902506c9d0f9d96b209
David Mardashev <begermsk@gmail.com>
2020-09-25 12:36:26 +0300

    Initial Commit

---
+++ DemoTodoList/DemoTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTodoListApp.swift
+//  DemoTodoList
+//
+//  Created by David Mardashev on 25.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct DemoTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
